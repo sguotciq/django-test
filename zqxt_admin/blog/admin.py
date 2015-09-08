9384dcf2831ff33d0503abf57befbada0af602d0
sguotciq <sguotciq@163.com>
2015-09-08 23:16:34 +0800

    add class article & Persons

--- zqxt_admin/blog/admin.py
+++ zqxt_admin/blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Article
+from .models import Person
+ 
+ 
+# admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','pub_date','update_time',)
+ 
+admin.site.register(Article,ArticleAdmin)
+
+class PersonAdmin(admin.ModelAdmin):
+	list_display = ('full_name',)
+
+admin.site.register(Person,PersonAdmin)
--- zqxt_admin/blog/models.py
+++ zqxt_admin/blog/models.py
@@ -1,3 +1,25 @@
+# coding:utf-8
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
+ 
+@python_2_unicode_compatible
+class Article(models.Model):
+    title = models.CharField(u'标题', max_length=256)
+    content = models.TextField(u'内容')
+ 
+    pub_date = models.DateTimeField(u'发表时间', auto_now_add=True, editable = True)
+    update_time = models.DateTimeField(u'更新时间',auto_now=True, null=True)
 
-# Create your models here.
+    def __str__(self):# 在Python3中用 __str__ 代替 __unicode__
+        return self.title
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+ 
+    def my_property(self):
+        return self.first_name + ' ' + self.last_name
+    my_property.short_description = "Full name of the person"
+ 
+    full_name = property(my_property)
