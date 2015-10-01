f6e242a88dc1b4c02471a1291f7d5f79571e898a
sguotciq <sguotciq@163.com>
2015-10-02 02:41:34 +0800

    json to js

--- mysite/learn/views.py
+++ mysite/learn/views.py
@@ -1,8 +1,9 @@
 # -*-coding:utf-8-*-
+from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-
+import json
 from django.http import HttpResponse
  
 def index(request):
@@ -12,7 +13,9 @@ def home(request):
 	string = u"我在自强学堂学习Django，用它来建网站"
 	TutorialList = ["HTML", "CSS", "jQuery", "Python", "Django"]
 	info_dict = {'site': u'自强学堂', 'content': u'各种IT技术教程'}
-	return render(request, 'home.html', {'string':string,'TutorialList':TutorialList,'info_dict': info_dict,})
+	List = ['自强学堂', '渲染Json到模板']
+	Dict = {'site': '自强学堂', 'author': '涂伟忠'}
+	return render(request, 'home.html', {'string':string,'TutorialList':TutorialList,'info_dict': info_dict, 'List':json.dumps(List), 'Dict':json.dumps(Dict),})
 
 def add(request):
 	a = request.GET['a']
