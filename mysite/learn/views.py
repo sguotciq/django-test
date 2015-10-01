a8ecc93c8b983a1d8b44c0b8401f80643610617d
sguotciq <sguotciq@163.com>
2015-10-02 02:14:21 +0800

    more abouttemplates

--- mysite/learn/views.py
+++ mysite/learn/views.py
@@ -9,7 +9,10 @@ def index(request):
     return HttpResponse(u"欢迎光临 自强学堂!")
 
 def home(request):
-	return render(request, 'home.html')
+	string = u"我在自强学堂学习Django，用它来建网站"
+	TutorialList = ["HTML", "CSS", "jQuery", "Python", "Django"]
+	info_dict = {'site': u'自强学堂', 'content': u'各种IT技术教程'}
+	return render(request, 'home.html', {'string':string,'TutorialList':TutorialList,'info_dict': info_dict,})
 
 def add(request):
 	a = request.GET['a']
