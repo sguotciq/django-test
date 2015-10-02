116c7e01cf4bed7d66fe27442467de09cb799dd0
sguotciq <sguotciq@163.com>
2015-10-02 16:05:02 +0800

    QureySet API

--- mysite/learn/models.py
+++ mysite/learn/models.py
@@ -16,3 +16,32 @@ class Article(models.Model):
 	content = models.CharField(max_length=3000)
 	def __unicode__(self):# 在Python3中使用 def __str__(self):
 		return self.title
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+ 
+    def __unicode__(self):  # __str__ on Python 3
+        return self.name
+ 
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+ 
+    def __unicode__(self):  # __str__ on Python 3
+        return self.name
+ 
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    n_comments = models.IntegerField()
+    n_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
+ 
+    def __unicode__(self):  # __str__ on Python 3
+        return self.headline
